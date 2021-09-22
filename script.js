d8a8f9c3b8f3a22cfd9ac5ab0b09bfdae09950d6
RosiePlatten <r.v.platten@gmail.com>
2021-09-22 15:48:57 +0100

    Added basic calculator functions, add, subtract, divide, multiply and operate

---
+++ script.js
@@ -0,0 +1,33 @@
+function add (a,b){
+    return a+b;
+}
+
+function subtract (a,b){
+    return a-b;
+}
+
+function multiply (a,b){
+    return a*b;
+}
+
+function divide (a,b){
+    return a/b;
+}
+
+function operate (operator,a,b){
+    switch(operator){
+        case "+":
+            return add(a,b);
+        case "-":
+            return subtract(a,b);
+        case "*":
+            return multiply(a,b);
+        case "/":
+            return divide(a,b);
+    }
+}
+
+console.log(add(1,1));
+console.log(subtract(10,5));
+console.log(multiply(4,2));
+console.log(divide(4,4));
